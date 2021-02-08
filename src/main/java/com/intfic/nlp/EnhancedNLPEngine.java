868748f47ff9b5569e232baa4ae4f77fbd73572b
ohnoah <noah.ohrner@yahoo.com>
2021-02-08 16:10:28 +0100

    working coref

--- src/main/java/com/intfic/nlp/EnhancedNLPEngine.java
+++ src/main/java/com/intfic/nlp/EnhancedNLPEngine.java
@@ -4,10 +4,11 @@ import com.intfic.game.basic.BasicGameEngine;
 import com.intfic.game.shared.ActionFormat;
 import com.intfic.game.shared.InstantiatedGameAction;
 import com.intfic.game.shared.Item;
+import edu.stanford.nlp.coref.CorefCoreAnnotations;
+import edu.stanford.nlp.coref.data.CorefChain;
 import edu.stanford.nlp.ling.CoreAnnotations;
 import edu.stanford.nlp.ling.CoreLabel;
 import edu.stanford.nlp.pipeline.CoreDocument;
-import edu.stanford.nlp.pipeline.CoreSentence;
 import edu.stanford.nlp.pipeline.StanfordCoreNLP;
 import edu.stanford.nlp.trees.Constituent;
 import edu.stanford.nlp.trees.LabeledScoredConstituentFactory;
@@ -15,9 +16,11 @@ import edu.stanford.nlp.trees.Tree;
 import edu.stanford.nlp.trees.TreeCoreAnnotations;
 import edu.stanford.nlp.util.Pair;
 import java.util.ArrayList;
-import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
 import java.util.Objects;
 import java.util.Properties;
 import java.util.Set;
@@ -31,14 +34,14 @@ public class EnhancedNLPEngine {
    private static StanfordCoreNLP pipeline;
 
 
-   private static List<Pair<Integer, Integer>> vpIndices(Tree tree) {
+   private static void npVpIndices(Tree tree, List<Pair<Integer, Integer>> vps, List<Pair<Integer, Integer>> nps) {
       Set<Constituent> treeConstituents = tree.constituents(new LabeledScoredConstituentFactory());
-      List<Pair<Integer, Integer>> vps = new ArrayList<>();
       List<Constituent> vpsConstituent = new ArrayList<>();
+      List<Constituent> npsConstituent = new ArrayList<>();
       outer:
       for (Constituent constituent : treeConstituents) {
          if (constituent.label() != null &&
-             (constituent.label().toString().equals("VP"))) {
+             (constituent.label().toString().equals("VP") || (constituent.label().toString().equals("NP")))) {
             for (int j = 0; j < vpsConstituent.size(); j++) {
                Constituent c = vpsConstituent.get(j);
                if (c != null && c.contains(constituent)) {
@@ -50,13 +53,25 @@ public class EnhancedNLPEngine {
                }
             }
             //System.err.println("found constituent: " + constituent.toString());
-            vps.add(new Pair<>(constituent.start(), constituent.end()));
-            vpsConstituent.add(constituent);
+            if (constituent.label().toString().equals("VP")) {
+               vps.add(new Pair<>(constituent.start(), constituent.end()));
+               vpsConstituent.add(constituent);
+            }
+            else if (constituent.label().toString().equals("NP")) {
+               nps.add(new Pair<>(constituent.start(), constituent.end()));
+               npsConstituent.add(constituent);
+            }
             //System.err.println(tree.getLeaves().subList(constituent.start(), constituent.end() + 1));
          }
       }
-      vps = vps.stream().filter(x -> !Objects.isNull(x)).collect(Collectors.toList());
-      return vps;
+      boolean status = true;
+      while (status) {
+         status = vps.remove(null);
+      }
+      status = true;
+      while (status) {
+         status = nps.remove(null);
+      }
    }
 
    public static List<InstantiatedGameAction> parseMultiple(@NotNull String rawCommand, @NotNull List<ActionFormat> possibleActionFormats, Set<Item> possibleItems) throws FailedParseException {
@@ -106,10 +121,52 @@ public class EnhancedNLPEngine {
       Tree tree =
           document.annotation().get(CoreAnnotations.SentencesAnnotation.class).get(0).get(TreeCoreAnnotations.TreeAnnotation.class);
       System.out.println(tree);
-      List<Pair<Integer, Integer>> vps = vpIndices(tree);
+      List<Pair<Integer, Integer>> vps = new ArrayList<>();
+      List<Pair<Integer, Integer>> nps = new ArrayList<>();
+      npVpIndices(tree, vps, nps);
+      Comparator<Pair<Integer, Integer>> pairComparator = (o1, o2) -> {
+         if (o1 == null && o2 == null) {
+            return 0;
+         }
+         else if (o1 == null && o2 != null) {
+            return -1;
+         }
+         else if (o1 != null && o2 == null) {
+            return 1;
+         }
+         if (o1.first.compareTo(o2.first) > 0) {
+            return 1;
+         }
+         else if (o1.first.compareTo(o2.first) < 0) {
+            return -1;
+         }
+         else {
+            return o1.second.compareTo(o2.second);
+         }
+      };
+      vps.sort(pairComparator);
+      nps.sort(pairComparator);
+      // TODO: NP indices
       System.out.println(vps);
+      System.out.println(nps);
 
-      // TODO: Check that no negations occur between vpIndices
+      // TODO: Check that no negations occur between vpIndices or that a separatae sentence
+
+      Map<Integer, CorefChain> corefChainMap = document.corefChains();
+      Map<Pair<Integer, Integer>, Pair<Integer, Integer>> representativeCorefCluster = new HashMap<>();
+      for (Map.Entry<Integer, CorefChain> entry : corefChainMap.entrySet()) {
+         CorefChain c = entry.getValue();
+         List<CorefChain.CorefMention> corefMentions = c.getMentionsInTextualOrder();
+         Pair<Integer, Integer> representing = new Pair<>(corefMentions.get(0).startIndex, corefMentions.get(0).endIndex);
+         for (CorefChain.CorefMention mention : c.getMentionsInTextualOrder()) {
+            Pair<Integer, Integer> mentionIndices = new Pair<>(mention.startIndex, mention.endIndex);
+            if (!representing.equals(mentionIndices)) {
+               representativeCorefCluster.put(mentionIndices, representing);
+               break;
+            }
+         }
+      }
+
 
       List<InstantiatedGameAction> commands = new ArrayList<>();
       for (Pair<Integer, Integer> vpStartEnd : vps) {
@@ -122,7 +179,11 @@ public class EnhancedNLPEngine {
 
          List<String> nouns = new ArrayList<>();
          List<Set<String>> adjectives = new ArrayList<>();
-         findNounsAndAdjectives(document, start, end, actionFormat, nouns, adjectives);
+/*         if(equalNouns.containsKey(vpStartEnd)){
+            nouns = equalNouns.get(vpStartEnd);
+            adjectives = equalAdjectives.get(vpStartEnd);
+         }*/
+         findNounsAndAdjectives(document, start, end, nps, actionFormat, nouns, adjectives, representativeCorefCluster);
 
          //TODO: do the wordnet stuff here for nouns
          List<String> gameItemNames = findMatchingGameItemNames(nouns, adjectives, possibleItems);
@@ -132,7 +193,8 @@ public class EnhancedNLPEngine {
          commands.add(command);
       }
       for (CoreLabel tok : document.tokens()) {
-         System.out.println(String.format("%s\t%s", tok.word(), tok.tag()));
+         String tagCoref = tagWithCoref(tok);
+         System.out.println(String.format("%s\t%s(%s)", tok.word(), tok.tag(), tagCoref.equals(tok.tag()) ? "-" : tagCoref));
       }
       System.out.println(document.corefChains());
       // Use that to look for a VB and a NN and populate a Command
@@ -190,25 +252,80 @@ public class EnhancedNLPEngine {
       return itemAdjectives.containsAll(userAdjectives);
    }
 
+   private static boolean isAdjective(String tag) {
+      return tag.equals("JJ") || tag.equals("JJR") || tag.equals("JJS");
+   }
+
+   private static String nounsAndAdjectivesInNP(CoreDocument document, Pair<Integer, Integer> np,
+                                                Set<String> adjectives, Map<Pair<Integer, Integer>, Pair<Set<String>, String>> corefCache,
+                                                Map<Pair<Integer, Integer>, Pair<Integer, Integer>> corefRepresentative) throws FailedParseException {
+
+
+      if (corefRepresentative.containsKey(np)) {
+         Pair<Integer, Integer> representativeNp = corefRepresentative.get(np);
+         Pair<Set<String>, String> adjAndNouns = corefCache.get(representativeNp);
+         adjectives.addAll(adjAndNouns.first);
+         return adjAndNouns.second;
+      }
+      List<CoreLabel> tokens = document.tokens();
+      for (int i = np.first; i <= np.second; i++) {
+         CoreLabel tok = tokens.get(i);
+         String tag = tok.tag();
+         System.out.println(tag);
+         if (isAdjective(tag)) {
+            adjectives.add(tok.word());
+         }
+         if (isNoun(tag)) {
+            String noun = tok.word();
+            corefCache.put(np, new Pair<>(adjectives, noun));
+            return noun;
+         }
+      }
+
+      throw new FailedParseException(String.format("Couldn't find any item in words: %s",
+          tokens.subList(np.first, np.second + 1).stream().map(CoreLabel::word).collect(Collectors.joining(" "))));
+   }
+
    // TODO: Rule out put the box in the box
-   public static void findNounsAndAdjectives(CoreDocument document, int start, int end, ActionFormat actionToTake, List<String> nouns, List<Set<String>> adjectives) throws FailedParseException {
+   public static void findNounsAndAdjectives(CoreDocument document, int start, int end,
+                                             List<Pair<Integer, Integer>> nps,
+                                             ActionFormat actionToTake, List<String> nouns,
+                                             List<Set<String>> adjectives, Map<Pair<Integer, Integer>, Pair<Integer, Integer>> corefRepresentative) throws FailedParseException {
       // TODO: Both cases should match against possibleActionNames with Wordnet
       // Word net in here
       // Either do a regex match for PUT IN
+      Map<Pair<Integer, Integer>, Pair<Set<String>, String>> corefCache = new HashMap<>();
+
+      List<Pair<Integer, Integer>> npsInVP = nps.stream().filter(np -> start <= np.first && np.second <= end).collect(Collectors.toList());
+      if (npsInVP.size() != actionToTake.getDegree()) {
+         throw new FailedParseException(String.format("Expected %d arguments for verb %s but got %d",
+             actionToTake.getDegree(), actionToTake.getVerb(), npsInVP.size()));
+      }
+
       if (actionToTake.isTernary()) {
          Pattern p = Pattern.compile(actionToTake.getRegExpr());
-         List<String> matchedStrings = document.tokens().subList(start, end + 1).stream().map(CoreLabel::value).collect(Collectors.toList());
+         List<CoreLabel> tokens = document.tokens();
+         List<String> matchedStrings = tokens.subList(start, end + 1).stream().map(CoreLabel::value).collect(Collectors.toList());
 
          Matcher m = p.matcher(String.join(" ", matchedStrings));
          boolean doesMatch = m.matches();
          if (doesMatch) {
+            int offset = tokens.get(start).beginPosition();
             for (int i = 1; i <= m.groupCount(); i++) {
                String matchingGroup = m.group(i);
-               appendMatchingGroupNoun(nouns, adjectives, document, matchingGroup, actionToTake, start, end, m.start(i), m.end(i));
+               Pair<Integer, Integer> np = npsInVP.get(i - 1);
+               int npStartCharIndex = tokens.get(np.first).beginPosition() - offset;
+               int npEndCharIndex = tokens.get(np.second).endPosition() - offset;
+               int matcherStart = m.start(i);
+               int matcherEnd = m.end(i);
+               if (npStartCharIndex != matcherStart || npEndCharIndex != matcherEnd) {
+                  throw new FailedParseException(String.format("Non-noun phrase specified in the %d:th slot of verb %s", i, actionToTake.getVerb()));
+               }
+               appendMatchingGroupNoun(nouns, adjectives, document, np, corefCache, corefRepresentative);
             }
          }
          else {
-            throw new FailedParseException(String.format("Argument structure after the %s was wrong.", actionToTake.getVerb()));
+            throw new FailedParseException(String.format("Argument structure after the verb %s was wrong.", actionToTake.getVerb()));
          }
       }
       // Or just find the noun
@@ -216,11 +333,15 @@ public class EnhancedNLPEngine {
          String noun;
          Set<String> currentAdjectives = new HashSet<>();
          List<CoreLabel> tokens = document.tokens();
-         for (int i = start; i <= end; i++) {
+         Pair<Integer, Integer> np = nps.get(0);
+         noun = nounsAndAdjectivesInNP(document, np, currentAdjectives, corefCache, corefRepresentative);
+         nouns.add(noun);
+         adjectives.add(currentAdjectives);
+/*         for (int i = np.first; i <= np.second; i++) {
             CoreLabel tok = tokens.get(i);
             String tag = tok.tag();
-            System.err.println(tag);
-            if (tag.equals("JJ") || tag.equals("JJR") || tag.equals("JJS")) {
+            System.out.println(tag);
+            if (isAdjective(tag)) {
                currentAdjectives.add(tok.word());
             }
             if (isNoun(tag)) {
@@ -229,10 +350,7 @@ public class EnhancedNLPEngine {
                nouns.add(noun);
                break;
             }
-         }
-         if (nouns.size() == 0) {
-            throw new FailedParseException("No noun specified");
-         }
+         }*/
       }
    }
 
@@ -250,18 +368,33 @@ public class EnhancedNLPEngine {
       return tag.equals("NN") || tag.equals("NNS") || tag.equals("NNP") || tag.equals("NNPS");
    }
 
+   private static String tagWithCoref(CoreLabel tok) {
+/*      for (Map.Entry<Integer, CorefChain> c : corefchains.entrySet()) {
+         CorefChain val = c.getValue();
+         System.out.println(val.getChainID());
+         System.out.println(val.getMentionMap());
+         System.out.println(val.getMentionsInTextualOrder());
+         System.out.println(val.getRepresentativeMention());
+         CorefChain.CorefMention mention =
+
+      }*/
+      return tok.tag();
+   }
+
    // TODO: How does this work if there is a "-" in the sentence
    private static void appendMatchingGroupNoun(List<String> nouns, List<Set<String>> adjectives,
-                                               CoreDocument document, String matchingGroup,
-                                               ActionFormat actionToTake, int tokenStart, int tokenEnd, int start, int end) throws FailedParseException {
+                                               CoreDocument document, Pair<Integer, Integer> np,
+                                               Map<Pair<Integer, Integer>, Pair<Set<String>, String>> corefCache,
+                                               Map<Pair<Integer, Integer>, Pair<Integer, Integer>> corefRepresentative) throws FailedParseException {
       /*      CoreDocument matchingDoc = generateCoreDocumentFromString(matchingGroup);*/
       /*      CoreDocument matchingDoc = generateCoreDocumentFromString(matchingGroup);*/
 /*      int index = Collections.indexOfSubList(document.tokens().stream().map(x -> removeNumberTag(x.toString())).collect(Collectors.toList()),
           matchingDoc.tokens().stream().map(x -> removeNumberTag(x.toString())).collect(Collectors.toList()));*/
 
-      int firstTokenIndex = -1;
-      int offset = document.tokens().get(tokenStart).beginPosition();
-      for (int i = tokenStart; i <= tokenEnd; i++) {
+/*      int firstTokenIndex = np.first;
+      int lastCharPos = document.tokens().get(np.second).endPosition();
+      int offset = document.tokens().get(np.first).beginPosition();
+*//*     for (int i = npStart; i <= npEnd; i++) {
          CoreLabel tok = document.tokens().get(i);
          if (tok.beginPosition() - offset == start) {
             firstTokenIndex = i;
@@ -272,16 +405,16 @@ public class EnhancedNLPEngine {
          System.err.println(document.tokens());
          System.err.println(matchingGroup);
          throw new FailedParseException("Incorrect usage of the verb: " + actionToTake.getVerb());
-      }
+      }*//*
 
       String noun;
       Set<String> currentAdjectives = new HashSet<>();
       int tokenIndex = firstTokenIndex;
       CoreLabel tok = document.tokens().get(tokenIndex);
-      while (tok.endPosition() - offset <= end) {
+      while (tok.endPosition() - offset <= lastCharPos) {
          tok = document.tokens().get(tokenIndex);
-         String tag = tok.tag();
-         if (tag.equals("JJ") || tag.equals("JJR") || tag.equals("JJS")) {
+         String tag = tagWithCoref(tok);
+         if (isAdjective(tag)) {
             currentAdjectives.add(tok.word());
          }
          if (isNoun(tag)) {
@@ -291,9 +424,13 @@ public class EnhancedNLPEngine {
             return;
          }
          tokenIndex++;
-      }
+      }*/
+      Set<String> currentAdjectives = new HashSet<>();
+      String noun = nounsAndAdjectivesInNP(document, np, currentAdjectives, corefCache, corefRepresentative);
+      nouns.add(noun);
+      adjectives.add(currentAdjectives);
 
-      throw new FailedParseException(String.format("Couldn't find any item in words: %s", matchingGroup));
+      //throw new FailedParseException(String.format("Couldn't find any item in words: %s", matchingGroup));
    }
 
    // This fails for e.g. TURN it ON, TURN the box
@@ -312,12 +449,12 @@ public class EnhancedNLPEngine {
       String verb;
       for (int i = start; i <= end; i++) {
          CoreLabel tok = document.tokens().get(i);
-         String tag = tok.tag();
+         String tag = tagWithCoref(tok);
          if (tag.equals("VB") || tag.equals("VBN") || tag.equals("VBG") || tag.equals("VBP") || tag.equals("VBZ")) {
             verb = tok.word();
             if (i != document.tokens().size() - 1) {
                CoreLabel nextToken = document.tokens().get(i + 1);
-               if (nextToken.tag().equals("RP")) {
+               if (tagWithCoref(nextToken).equals("RP")) {
                   verb += String.format(" %s", nextToken.word());
                }
             }
@@ -379,9 +516,12 @@ public class EnhancedNLPEngine {
              .parse("put the key in the box",
                  new BasicGameEngine().getPossibleActionFormats(), Set.of(new Item("key"), new Item("box"))).get(0);
          InstantiatedGameAction command2 = EnhancedNLPEngine
-             .parse("put the box in the key",
+             .parse("put the box in the key and eat the key",
                  new BasicGameEngine().getPossibleActionFormats(), Set.of(new Item("key"), new Item("box"))).get(0);
          InstantiatedGameAction command3 = EnhancedNLPEngine
+             .parse("put the donkey in the box and eat it. eat box.",
+                 new BasicGameEngine().getPossibleActionFormats(), Set.of(new Item("key"), new Item("donkey"), new Item("box"))).get(0);
+         InstantiatedGameAction command4 = EnhancedNLPEngine
              .parse("put the donkey in the box and drink it from the box. eat box.",
                  new BasicGameEngine().getPossibleActionFormats(), Set.of(new Item("key"), new Item("donkey"), new Item("box"))).get(0);
       }
